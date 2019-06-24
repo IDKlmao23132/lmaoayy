53c80d2dfd3599da7f5d41d01ff1b6180ddaf8d6
IDKlmao23132 <52093751+IDKlmao23132@users.noreply.github.com>
2019-06-24 21:32:00 +0700

    Add files via upload

---
+++ dist/config.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = {
+    "token": "NTg3MjE3NTUxMjEwMTg0NzU0.XPzXGg.EjHUZEPh_oA4pqD19owYWNB2gGY",
+    "prefix": "catgod!",
+    "commands": [
+        "kick",
+        "ban"
+    ]
+};
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uZmlnLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vc3JjL2NvbmZpZy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFXLFFBQUEsTUFBTSxHQUFHO0lBQ2hCLE9BQU8sRUFBRSw2REFBNkQ7SUFDdEUsUUFBUSxFQUFFLFNBQVM7SUFDbkIsVUFBVSxFQUFFO1FBQ1IsTUFBTTtRQUNOLEtBQUs7S0FDUjtDQUNKLENBQUEifQ==
